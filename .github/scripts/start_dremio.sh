870473501727e2b6ac545c3db61557926e878867
Liliana Zheng <liliana.zheng@dremio.com>
2025-02-03 13:34:21 +0000

    Try running CI with locally running instance of Dremio

--- .github/scripts/start_dremio.sh
+++ .github/scripts/start_dremio.sh
@@ -1,14 +1,27 @@
+# #!/bin/bash
+# set -e
+
+# echo "Starting Dremio service..."
+
+# docker run -d \
+#   --network ci-network \
+#   --name dremio \
+#   -p 31010:31010 \
+#   -p 9047:9047 \
+#   -e "DREMIO_JAVA_SERVER_EXTRA_OPTS=-Ddebug.addDefaultUser=true" \
+#   dremio/dremio-oss
+
+# echo "Dremio service started."
+
 #!/bin/bash
 set -e
 
-echo "Starting Dremio service..."
+echo "Checking Dremio Enterprise service health..."
 
-docker run -d \
-  --network ci-network \
-  --name dremio \
-  -p 31010:31010 \
-  -p 9047:9047 \
-  -e "DREMIO_JAVA_SERVER_EXTRA_OPTS=-Ddebug.addDefaultUser=true" \
-  dremio/dremio-ee:latest
-
-echo "Dremio service started."
+# Check if Dremio is running by hitting the health endpoint
+if curl -s --head  --request GET http://localhost:9047 | grep "200 OK" > /dev/null; then 
+   echo "Dremio Enterprise service is running."
+else
+   echo "Dremio Enterprise service is not running. Please start the service."
+   exit 1
+fi
