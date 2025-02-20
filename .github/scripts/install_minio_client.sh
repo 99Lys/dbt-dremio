6d9661373199dcaf790b15993e339bafaf3fa5ae
Liliana Zheng <liliana.zheng@dremio.com>
2025-02-20 16:18:42 +0000

    remove sudo

--- .github/scripts/install_minio_client.sh
+++ .github/scripts/install_minio_client.sh
@@ -5,6 +5,6 @@ echo "Installing MinIO Client (mc)..."
 
 curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc
-sudo mv mc /usr/local/bin/
+mv mc /usr/local/bin/
 
 echo "MinIO Client installed successfully."
