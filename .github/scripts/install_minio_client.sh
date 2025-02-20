b78a9dc2ad7e9ca13e7529a4406e435fa01b3369
Liliana Zheng <liliana.zheng@dremio.com>
2025-02-20 15:37:21 +0000

    switch curl to wget trying to solve 400

--- .github/scripts/install_minio_client.sh
+++ .github/scripts/install_minio_client.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Installing MinIO Client (mc)..."
 
-curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
+wget https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc
 sudo mv mc /usr/local/bin/
 
