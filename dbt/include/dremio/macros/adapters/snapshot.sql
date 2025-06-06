a083b1baa88943dec95ba35a22890fae289b4d9b
Liliana Zheng <liliana.zheng@dremio.com>
2025-06-06 18:32:36 +0100

    Syntax update

--- dbt/include/dremio/macros/adapters/snapshot.sql
+++ dbt/include/dremio/macros/adapters/snapshot.sql
@@ -21,27 +21,24 @@ limitations under the License.*/
   using {{ source }} as DBT_INTERNAL_SOURCE
   on DBT_INTERNAL_SOURCE.{{ columns.dbt_scd_id }} = DBT_INTERNAL_DEST.{{ columns.dbt_scd_id }}
 
-  when matched
-    {%- if config.get("dbt_valid_to_current") %}
-      and (
-        DBT_INTERNAL_DEST.{{ columns.dbt_valid_to }} = {{ config.get('dbt_valid_to_current') }}
-        or DBT_INTERNAL_DEST.{{ columns.dbt_valid_to }} is null
-      )
-    {%- else %}
-      and DBT_INTERNAL_DEST.{{ columns.dbt_valid_to }} is null
-    {%- endif %}
-    and DBT_INTERNAL_SOURCE.dbt_change_type in ('update','delete')
-      then update
-      set {{ columns.dbt_valid_to }} = DBT_INTERNAL_SOURCE.{{ columns.dbt_valid_to }}
+  when matched then update
+    set {{ columns.dbt_valid_to }} = DBT_INTERNAL_SOURCE.{{ columns.dbt_valid_to }}
+    where (
+      {%- if config.get("dbt_valid_to_current") %}
+        (DBT_INTERNAL_DEST.{{ columns.dbt_valid_to }} = {{ config.get('dbt_valid_to_current') }}
+        or DBT_INTERNAL_DEST.{{ columns.dbt_valid_to }} is null)
+      {%- else %}
+        DBT_INTERNAL_DEST.{{ columns.dbt_valid_to }} is null
+      {%- endif %}
+      and DBT_INTERNAL_SOURCE.dbt_change_type in ('update','delete')
+    )
 
-  when not matched
-    and DBT_INTERNAL_SOURCE.dbt_change_type = 'insert'
-    then insert ({{ insert_cols_csv }})
-    values
-    (
+  when not matched then insert ({{ insert_cols_csv }})
+    values (
       {%- for column_name in insert_cols -%}
         DBT_INTERNAL_SOURCE.{{ column_name }}{% if not loop.last %}, {% endif %}
       {%- endfor %}
     )
+    where DBT_INTERNAL_SOURCE.dbt_change_type = 'insert'
 
 {% endmacro %}
